cff26897d409c9780c7232a3ff4f1e95c61d9aa7
Evandro oliveira <vandoaparecido@hotmail.com>
2022-08-21 20:50:29 -0300

    :rocket: Validando cpf

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ app.use(express.json());
 const customers = [];
 
 app.post("/account", (req, res) => {
+
     const { cpf, name } = req.body;
-    const id = uuidv4()
-   
+     const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if (customerAlreadyExists) {
+        return res.status(400).json({
+            error: "Customer already exists!"
+        });
+    }   
+    
     customers.push({
         cpf,
         name,
