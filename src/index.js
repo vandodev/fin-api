4807248882711f12fa6a88b11c7ffc33f7e9cd1a
Evandro oliveira <vandoaparecido@hotmail.com>
2022-08-22 20:21:41 -0300

    :rocket: Validando conta não existente

--- src/index.js
+++ src/index.js
@@ -31,6 +31,13 @@ app.post("/account", (req, res) => {
 app.get("/statement/:cpf", (req, res) => {
     const {cpf} = req.params;
     const customer = customers.find(customer => customer.cpf === cpf);
+
+     if (!customer) {
+        return res.status(400).json({
+            error: "Customer not found!"
+        });
+    } 
+
     return res.json(customer.statement);
 });
 
