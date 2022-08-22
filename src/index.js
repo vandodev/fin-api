ceeea720152a3ea488d71ee67476bedc06e7252c
Evandro oliveira <vandoaparecido@hotmail.com>
2022-08-22 20:11:10 -0300

    :rocket: Listando extrato

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -25,6 +26,12 @@ app.post("/account", (req, res) => {
     });
 
     return res.status(201).send();
+});
+
+app.get("/statement/:cpf", (req, res) => {
+    const {cpf} = req.params;
+    const customer = customers.find(customer => customer.cpf === cpf);
+    return res.json(customer.statement);
 });
 
 app.listen(3333, () => {
